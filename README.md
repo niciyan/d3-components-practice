1966e3b22f22f8b386dfd65c9c45fa7cf1f52d31
niciyan <nishiyamasu@gmail.com>
2018-07-08 17:56:43 +0900

    use vector is good.

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 ## Practice repo
 
 practice of [ d3.js ](https://d3js.org/)
+
+You may need npm install.
+
+	$ npm install
--- curve.js
+++ curve.js
@@ -1,33 +1,49 @@
 var curve = document.getElementById("curve");
-var ax = 20;
-var ay = 20;
-var bx = 400;
-var by = 400;
-var cx = 500;
-var cy = 200;
+var point_d = document.getElementById("point_d");
+var point_a = document.getElementById("point_a");
+var point_p = document.getElementById("point_p");
+
+var ad = document.getElementById("ad");
+var pd = document.getElementById("pd");
 
 var t = 0;
 var dt = 0.01;
 
-// var ab_x = bx-ax;
-// var ab_y = by-ay;
+var a = new Victor(20,20);
+var b = new Victor(400, 400);
+var c = new Victor(500, 200);
+
+point_a.setAttribute("transform", translate(a.x,a.y));
 
 function next(){
 	t += dt;
-	if (t > 1){
-		t = 0;
-	}
-	ab_x = (1-t)*ax + t*cx;
-	ab_y = (1-t)*ay + t*cy;
-	dst_x = (1-t)*(1-t)*ax + t*t*bx + 2*t*(1-t)*cx;
-	dst_y = (1-t)*(1-t)*ay + t*t*by + 2*t*(1-t)*cy;
-	curve.setAttribute("d", path_curve(ax,ay,ab_x,ab_y,dst_x,dst_y));
+	
+	// if (t > 1){
+	// 	t = 0;
+	// }
+
+	(function(t){
+		var d = a.clone().multiplyScalar(1-t).add(c.clone().multiplyScalar(t)) ;
+		var e = b.clone().multiplyScalar(t).add(c.clone().multiplyScalar(1-t));
+		var p = d.clone().multiplyScalar(1-t).add(e.clone().multiplyScalar(t));
+		curve.setAttribute("d", path_curve(a.x,a.y,d.x,d.y,p.x,p.y));
+		point_d.setAttribute("transform", translate(d.x,d.y));
+		point_p.setAttribute("transform", translate(p.x,p.y));
+		ad.setAttribute("d", path_line(a.x,a.y,d.x,d.y));
+		pd.setAttribute("d", path_line(d.x,d.y,p.x,p.y));
+	})(Math.abs( Math.sin(t) ));
 
 	requestAnimationFrame(next);
 }
 
 function path_curve(x0,y0,x1,y1,x2,y2){
 	return "M"+x0+" "+y0+" Q"+x1+" "+y1+" "+x2+" "+y2;
+}
+function translate(x,y){
+	return "translate("+x+","+y+")";
+}
+function path_line(x0,y0,x1,y1){
+	return "M"+x0+" "+y0+" L"+x1+" "+y1;
 }
 
 next();
