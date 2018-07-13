2b66178d497c2ac40ccd5b570f027916798231e8
niciyan <nishiyamasu@gmail.com>
2018-07-13 22:07:01 +0900

    interactive

--- tree.js
+++ tree.js
@@ -1,18 +1,19 @@
 var family = [
-{ name:"nishi", parent:"" },
-{ name:"Taka", parent:"nishi"},
-{ name:"Suzu", parent:"nishi"},
-{ name:"Yuzu", parent:"nishi"},
-{ name:"Kevi", parent:"nishi"},
-{ name:"Goke", parent:"Yuzu"},
-{ name:"Duke", parent:"Goke"},
-{ name:"たか", parent:"Goke"},
-{ name:"hero", parent:"Goke"},
-{ name:"Roger Federer", parent:"Goke"},
-{ name:"Federer", parent:"Goke"},
-{ name:"Rafael", parent:"Goke"},
-{ name:"Zeke", parent:"Yuzu"},
+{ name:"nishi",   parent:""     ,value:1280},
+{ name:"Taka",    parent:"nishi",value:2226},
+{ name:"Suzu",    parent:"nishi",value:3286},
+{ name:"Yuzu",    parent:"nishi",value:4242},
+{ name:"Kevi",    parent:"nishi",value:5320},
+{ name:"Goke",    parent:"Yuzu" ,value:6579},
+{ name:"Duke",    parent:"Goke" ,value:7106},
+{ name:"たか",    parent:"Goke" ,value:8213},
+{ name:"hero",    parent:"Goke" ,value:9904},
+{ name:"Roger",   parent:"Goke" ,value:1762 },
+{ name:"Federer", parent:"Goke" ,value:1506 },
+{ name:"Rafael",  parent:"Goke" ,value:1192 },
+{ name:"Zeke",    parent:"Yuzu" ,value:1240 },
 ]
+
 
 var stratify = d3.stratify()
 	.id(function(d){return d.name})
@@ -40,7 +41,7 @@ var link = svg.selectAll().data(root.descendants().slice(1))
 
 var node = svg.selectAll().data(root.descendants())
 	.enter().append("g")
-	.attr("transform", function(d){ return translate(d.x,d.y) });
+	.attr("transform", function(d){ return translate(d.x,d.y) })
 
 node.append("circle")
 	.attr("stroke", "black")
@@ -50,11 +51,19 @@ node.append("circle")
 node.append("text")
 	.attr("dx", 20)
 	.attr("dy", 20)
-	.text(function(d){ return d.id; });
+	.text(function(d){ return d.id; })
+	.on("mouseover", start)
+	.on("mouseleave", end);
 
 function translate(x,y){
 	return "translate("+x+","+y+")";
 }
 function path_curve3(x0,y0,x1,y1,x2,y2,x3,y3){
 	return "M"+x0+" "+y0+" C"+x1+" "+y1+" "+x2+" "+y2+" "+x3+" "+y3;
+}
+function start(){
+	d3.select(this).text(function(d){ return d.parent ? "parent: "+d.parent.id+" id: "+d.id : "id: "+d.id; });
+}
+function end(){
+	d3.select(this).text(function(d){ return d.id; });
 }
